ebceed018083c21715c6440eca0b9dc3aaa0911b
semaki2000 <62149867+semaki2000@users.noreply.github.com>
2023-09-01 10:39:35 +0200

    rafactoring calculator-example works for exact clones (simple solution)

--- refactoring_test/refactor_type1_clones.py
+++ refactoring_test/refactor_type1_clones.py
@@ -3,9 +3,9 @@ import astor;
 
 
 
-
 def main():
-    ast_tree = parse_file_to_AST("../test_files/calculator_test_type1clone.py")
+    filename = "../test_files/calculator_test_type1clone.py"
+    ast_tree = parse_file_to_AST(filename)
 
     clone_names : list = [("test_addition", "test_addition2")] #list with lists of matching clones
     flattened_clone_names = [item for subtuple in clone_names for item in subtuple]
@@ -15,7 +15,9 @@ def main():
     
     print("Test:", len(matched_clone_pairs))
 
-    ast_rewrite_extract_method()
+    ast_refactor_type1_clones(matched_clone_pairs)
+
+    parse_AST_to_file(ast_tree, filename.split(".")[0] + "_refactored.py")
 
 
 def parse_file_to_AST(filename):
@@ -53,10 +55,31 @@ def sort_clones_into_matched_clone_pairs(nodes, names):
 
 
 def ast_refactor_type1_clones(nodes):
+    #type 1 clones == exact clone
+    #example solution: replace implementation of clone1 with call to clone2
+    for clone_pair in nodes:
+        clone0 = clone_pair[0]
+        clone1 = clone_pair[1]
 
+
+        #ast.Call object must be wrapped in ast.Expr, 
+        #otherwise it will be added onto the same line as last node.
+        call_to_clone0 = ast.Expr(value=ast.Call(ast.Name(clone0.name), clone1.args.args, []))
+
+        
+        clone1.body = []
+        clone1.body.append(call_to_clone0)
+
+        print(clone0.body)
+        print(clone1.body)
+
+
+def parse_AST_to_file(ast_tree, filename: str):
     
-
-    pass
+    
+    string_ast = ast.unparse(ast_tree)
+    with open(filename, "w") as file:
+        file.write(string_ast)
 
 
 main()
