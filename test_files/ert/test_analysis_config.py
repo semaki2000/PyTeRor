db2fc3dd66d38fb94e1a83f607f9c7a2bd9f11bd
Sebastian Max Sjøvoll Kingston <semaki2000@gmail.com>
2023-10-13 13:42:31 +0200

    moved test files into separate subfolders

---
+++ test_files/ert/test_analysis_config.py
@@ -0,0 +1,297 @@
+from textwrap import dedent
+
+import hypothesis.strategies as st
+import pytest
+from hypothesis import given
+
+from ert.config import AnalysisConfig, ConfigValidationError, ErtConfig
+from ert.config.parsing import ConfigKeys, ConfigWarning
+
+
+@pytest.mark.usefixtures("use_tmpdir")
+def test_analysis_config_from_file_is_same_as_from_dict():
+    with open("analysis_config", "w", encoding="utf-8") as fout:
+        fout.write(
+            dedent(
+                """
+                NUM_REALIZATIONS 10
+                MIN_REALIZATIONS 10
+                ANALYSIS_SET_VAR STD_ENKF ENKF_NCOMP 2
+                """
+            )
+        )
+    analysis_config = ErtConfig.from_file("analysis_config").analysis_config
+    assert analysis_config == AnalysisConfig.from_dict(
+        {
+            ConfigKeys.NUM_REALIZATIONS: 10,
+            ConfigKeys.MIN_REALIZATIONS: "10",
+            ConfigKeys.ANALYSIS_SET_VAR: [
+                ("STD_ENKF", "ENKF_NCOMP", 2),
+            ],
+        }
+    )
+
+
+@pytest.mark.parametrize(
+    "num_realization, min_realizations, expected_min_real",
+    [
+        (80, "10%", 8),
+        (5, "2%", 1),
+        (8, "50%", 4),
+        (8, "100%", 8),
+        (8, "100%", 8),
+        (8, "10%", 1),
+        (80, "0", 80),
+        (80, "0", 80),
+        (900, "10", 10),
+        (80, "50", 50),
+        (80, "80", 80),
+        (80, "100", 80),
+    ],
+)
+def test_analysis_config_min_realizations(
+    num_realization, min_realizations, expected_min_real
+):
+    assert (
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: num_realization,
+                ConfigKeys.MIN_REALIZATIONS: min_realizations,
+            }
+        ).minimum_required_realizations
+        == expected_min_real
+    )
+
+
+def test_invalid_min_realization_raises_config_validation_error():
+    with pytest.raises(
+        ConfigValidationError, match="MIN_REALIZATIONS value is not integer"
+    ):
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: 1,
+                ConfigKeys.MIN_REALIZATIONS: "1s",
+            }
+        )
+
+
+def test_invalid_min_realization_percentage_raises_config_validation_error():
+    with pytest.raises(
+        ConfigValidationError,
+        match="MIN_REALIZATIONS 'd%s' contained % but was not a valid percentage",
+    ):
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: 1,
+                ConfigKeys.MIN_REALIZATIONS: "d%s",
+            }
+        )
+
+
+@pytest.mark.parametrize(
+    "value, expected", [("100%", 50), ("+34", 34), ("-1", -1), ("50.5%", 26)]
+)
+def test_valid_min_realization(value, expected):
+    assert (
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: 50,
+                ConfigKeys.MIN_REALIZATIONS: value,
+            }
+        ).minimum_required_realizations
+        == expected
+    )
+
+
+@pytest.mark.parametrize(
+    "analysis_config", [AnalysisConfig(), AnalysisConfig.from_dict({})]
+)
+def test_analysis_config_modules(analysis_config):
+    default_modules = analysis_config._modules
+    assert len(default_modules) == 2
+    assert "IES_ENKF" in default_modules
+    assert "STD_ENKF" in default_modules
+
+    assert analysis_config.active_module().name == "STD_ENKF"
+
+    assert analysis_config.select_module("IES_ENKF")
+
+    assert analysis_config.active_module().name == "IES_ENKF"
+
+    es_module = analysis_config.get_module("STD_ENKF")
+    assert es_module.name == "STD_ENKF"
+    with pytest.raises(
+        ConfigValidationError, match="Analysis module UNKNOWN not found!"
+    ):
+        analysis_config.get_module("UNKNOWN")
+    assert not analysis_config.select_module("UNKNOWN")
+
+
+def test_analysis_config_iter_config_dict_initialisation():
+    expected_case_format = "case_%d"
+    analysis_config = AnalysisConfig.from_dict(
+        {
+            ConfigKeys.NUM_REALIZATIONS: 10,
+            ConfigKeys.ITER_CASE: expected_case_format,
+            ConfigKeys.ITER_COUNT: 42,
+            ConfigKeys.ITER_RETRY_COUNT: 24,
+        }
+    )
+
+    assert analysis_config.case_format_is_set() is True
+    assert analysis_config.case_format == expected_case_format
+    assert analysis_config.num_iterations == 42
+    assert analysis_config.num_retries_per_iter == 24
+
+
+@pytest.mark.parametrize(
+    "analysis_config", [AnalysisConfig(), AnalysisConfig.from_dict({})]
+)
+def test_analysis_config_iter_config_default_initialisation(analysis_config):
+    assert analysis_config.num_iterations == 4
+    assert analysis_config.num_retries_per_iter == 4
+    analysis_config.set_num_iterations(42)
+    assert analysis_config.num_iterations == 42
+
+
+@pytest.mark.parametrize(
+    "analysis_config", [AnalysisConfig(), AnalysisConfig.from_dict({})]
+)
+def test_setting_case_format(analysis_config):
+    assert analysis_config.case_format is None
+    assert not analysis_config.case_format_is_set()
+    expected_case_format = "case_%d"
+    analysis_config.set_case_format(expected_case_format)
+    assert analysis_config.case_format == expected_case_format
+    assert analysis_config.case_format_is_set()
+
+
+def test_incorrect_variable_raises_validation_error():
+    with pytest.raises(
+        ConfigValidationError, match="Variable 'IES_INVERSION' with value 'FOO'"
+    ):
+        _ = AnalysisConfig.from_dict(
+            {
+                ConfigKeys.ANALYSIS_SET_VAR: [["STD_ENKF", "IES_INVERSION", "FOO"]],
+            }
+        )
+
+
+def test_unknown_variable_raises_validation_error():
+    with pytest.raises(
+        ConfigValidationError, match="Variable 'BAR' not found in 'STD_ENKF' analysis"
+    ):
+        _ = AnalysisConfig.from_dict(
+            {
+                ConfigKeys.ANALYSIS_SET_VAR: [["STD_ENKF", "BAR", "1"]],
+            }
+        )
+
+
+def test_default_alpha_is_set():
+    default_alpha = 3.0
+    assert AnalysisConfig.from_dict({}).enkf_alpha == default_alpha
+    assert AnalysisConfig().enkf_alpha == default_alpha
+
+
+@given(st.floats(allow_nan=False, allow_infinity=False))
+def test_alpha_is_set_from_corresponding_key(value):
+    assert AnalysisConfig.from_dict({ConfigKeys.ENKF_ALPHA: value}).enkf_alpha == value
+    assert AnalysisConfig(alpha=value).enkf_alpha == value
+
+
+@given(st.floats(allow_nan=False, allow_infinity=False))
+def test_analysis_config_alpha_set_and_get(value):
+    analysis_config = AnalysisConfig()
+    analysis_config.enkf_alpha = value
+    assert analysis_config.enkf_alpha == value
+
+
+def test_default_std_cutoff_is_set():
+    default_std_cutoff = 1e-6
+    assert AnalysisConfig.from_dict({}).std_cutoff == default_std_cutoff
+    assert AnalysisConfig().std_cutoff == default_std_cutoff
+
+
+@given(st.floats(allow_nan=False, allow_infinity=False))
+def test_std_cutoff_is_set_from_corresponding_key(value):
+    assert AnalysisConfig.from_dict({ConfigKeys.STD_CUTOFF: value}).std_cutoff == value
+    assert AnalysisConfig(std_cutoff=value).std_cutoff == value
+
+
+def test_default_max_runtime_is_unlimited():
+    assert AnalysisConfig.from_dict({}).max_runtime is None
+    assert AnalysisConfig().max_runtime is None
+
+
+@given(st.integers(min_value=1))
+def test_max_runtime_is_set_from_corresponding_keyword(value):
+    assert (
+        AnalysisConfig.from_dict({ConfigKeys.MAX_RUNTIME: value}).max_runtime == value
+    )
+    assert AnalysisConfig(max_runtime=value).max_runtime == value
+
+
+def test_default_stop_long_running_is_false():
+    assert not AnalysisConfig.from_dict({}).stop_long_running
+    assert not AnalysisConfig().stop_long_running
+
+
+@pytest.mark.parametrize("value", [True, False])
+def test_stop_long_running_is_set_from_corresponding_keyword(value):
+    assert (
+        AnalysisConfig.from_dict(
+            {ConfigKeys.STOP_LONG_RUNNING: value}
+        ).stop_long_running
+        == value
+    )
+    assert AnalysisConfig(stop_long_running=value).stop_long_running == value
+
+
+@given(st.integers(min_value=1))
+def test_default_min_realization_is_all_realizations(value):
+    assert (
+        AnalysisConfig.from_dict(
+            {ConfigKeys.NUM_REALIZATIONS: value}
+        ).minimum_required_realizations
+        == value
+    )
+
+
+@given(st.integers(min_value=1))
+def test_min_realization_is_set_from_corresponding_keyword(value):
+    assert (
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: value + 1,
+                ConfigKeys.MIN_REALIZATIONS: str(value),
+            }
+        ).minimum_required_realizations
+        == value
+    )
+    assert AnalysisConfig(min_realization=value).minimum_required_realizations == value
+
+
+def test_giving_larger_min_than_num_realizations_warns():
+    with pytest.warns(
+        ConfigWarning, match="MIN_REALIZATIONS set to more than NUM_REALIZATIONS"
+    ):
+        _ = AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: 1,
+                ConfigKeys.MIN_REALIZATIONS: "2",
+            }
+        )
+
+
+def test_num_realizations_0_means_all():
+    """For legacy reasons, `NUM_REALIZATIONS 0` means all must pass."""
+    assert (
+        AnalysisConfig.from_dict(
+            {
+                ConfigKeys.NUM_REALIZATIONS: 100,
+                ConfigKeys.MIN_REALIZATIONS: "0",
+            }
+        ).minimum_required_realizations
+        == 100
+    )
---
+++ test_files/ert/test_config_schema_deprecations.py
@@ -0,0 +1,228 @@
+import warnings
+
+import pytest
+
+from ert.config import ConfigWarning, ErtConfig
+from ert.config.parsing.config_schema_deprecations import (
+    JUST_REMOVE_KEYWORDS,
+    REPLACE_WITH_GEN_KW,
+    RSH_KEYWORDS,
+    USE_QUEUE_OPTION,
+)
+from ert.config.parsing.deprecation_info import DeprecationInfo
+
+
+def test_is_angle_bracketed():
+    assert DeprecationInfo.is_angle_bracketed("<KEY>")
+    assert not DeprecationInfo.is_angle_bracketed("KEY")
+    assert not DeprecationInfo.is_angle_bracketed("K<E>Y")
+    assert not DeprecationInfo.is_angle_bracketed("")
+
+
+def make_suggestion_list(path):
+    with warnings.catch_warnings(record=True) as all_warnings:
+        _ = ErtConfig.from_file(path)
+    return [
+        str(w.message)
+        for w in all_warnings
+        if w.category == ConfigWarning and w.message.info.is_deprecation
+    ]
+
+
+@pytest.mark.parametrize("kw", JUST_REMOVE_KEYWORDS)
+def test_that_suggester_gives_simple_migrations(tmp_path, kw):
+    (tmp_path / "config.ert").write_text(f"NUM_REALIZATIONS 1\n{kw}\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(f"The keyword {kw} no longer" in s for s in suggestions)
+
+
+def test_that_suggester_gives_havana_fault_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nHAVANA_FAULT\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The behavior of HAVANA_FAULT can be reproduced using" in s for s in suggestions
+    )
+
+
+@pytest.mark.parametrize("kw", REPLACE_WITH_GEN_KW)
+def test_that_suggester_gives_gen_kw_migrations(tmp_path, kw):
+    (tmp_path / "config.ert").write_text(f"NUM_REALIZATIONS 1\n{kw}\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "ert.readthedocs.io/en/latest/reference/configuration/keywords.html#gen-kw" in s
+        for s in suggestions
+    )
+
+
+@pytest.mark.parametrize("kw", RSH_KEYWORDS)
+def test_that_suggester_gives_rsh_migrations(tmp_path, kw):
+    (tmp_path / "config.ert").write_text(f"NUM_REALIZATIONS 1\n{kw}\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "deprecated and removed support for RSH queues." in s for s in suggestions
+    )
+
+
+@pytest.mark.parametrize("kw", USE_QUEUE_OPTION)
+def test_that_suggester_gives_queue_option_migrations(tmp_path, kw):
+    (tmp_path / "config.ert").write_text(f"NUM_REALIZATIONS 1\n{kw}\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        f"The {kw} keyword has been removed. For most cases " in s for s in suggestions
+    )
+
+
+def test_that_suggester_gives_refcase_list_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nREFCASE_LIST case.DATA\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The corresponding plotting functionality was removed in 2015" in s
+        for s in suggestions
+    )
+
+
+def test_that_suggester_gives_rftpath_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nRFTPATH rfts/\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The corresponding plotting functionality was removed in 2015" in s
+        for s in suggestions
+    )
+
+
+def test_that_suggester_gives_end_date_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nEND_DATE 2023.01.01\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any("only display a warning in case of problems" in s for s in suggestions)
+
+
+def test_that_suggester_gives_rerun_start_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nRERUN_START 2023.01.01\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "used for the deprecated run mode ENKF_ASSIMILATION" in s for s in suggestions
+    )
+
+
+def test_that_suggester_gives_delete_runpath_migration(tmp_path):
+    (tmp_path / "config.ert").write_text("NUM_REALIZATIONS 1\nDELETE_RUNPATH TRUE\n")
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any("It was removed in 2017" in s for s in suggestions)
+
+
+def test_suggester_gives_runpath_deprecated_specifier_migration(tmp_path):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nRUNPATH real-%d/iter-%d\n"
+    )
+    with pytest.warns(
+        ConfigWarning,
+        match="RUNPATH keyword contains deprecated value"
+        r" placeholders: %d, instead use: .*real-<IENS>\/iter-<ITER>",
+    ):
+        _ = ErtConfig.from_file(tmp_path / "config.ert")
+
+
+def test_suggester_gives_no_runpath_deprecated_specifier_migration(tmp_path):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nRUNPATH real-<IENS>/iter-<ITER>\n"
+    )
+    no_suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    (tmp_path / "config.wrong.ert").write_text(
+        "NUM_REALIZATIONS 1\nRUNPATH real-%d/iter-%d\n"
+    )
+    suggestions = make_suggestion_list(str(tmp_path / "config.wrong.ert"))
+
+    assert not any(
+        "RUNPATH keyword contains deprecated value placeholders" in s
+        for s in no_suggestions
+    ) and any(
+        "RUNPATH keyword contains deprecated value placeholders" in s
+        for s in suggestions
+    )
+
+
+def test_suggester_gives_plot_settings_migration(tmp_path):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nPLOT_SETTINGS some args\n"
+    )
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The keyword PLOT_SETTINGS was removed in 2019 and has no effect" in s
+        for s in suggestions
+    )
+
+
+def test_suggester_gives_update_settings_migration(tmp_path):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nUPDATE_SETTINGS some args\n"
+    )
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The UPDATE_SETTINGS keyword has been removed and no longer" in s
+        for s in suggestions
+    )
+
+
+@pytest.mark.parametrize("definer", ["DEFINE", "DATA_KW"])
+def test_suggester_gives_deprecated_define_migration_hint(tmp_path, definer):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\n"
+        f"{definer} <KEY1> x1\n"
+        f"{definer} A B\n"
+        f"{definer} <A<B>> C\n"
+        f"{definer} <A><B> C\n"
+    )
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+    assert len(suggestions) == 3
+    for suggestion, expected in zip(
+        suggestions,
+        [
+            " Please change A to <A>",
+            " Please change <A<B>> to <AB>",
+            " Please change <A><B> to <AB>",
+        ],
+    ):
+        assert (
+            f"Using {definer} with substitution strings"
+            " that are not of the form '<KEY>' is deprecated." in suggestion
+        )
+        assert suggestion.endswith(expected)
+
+
+def test_suggester_does_not_report_non_existent_path_due_to_missing_pre_defines(
+    tmp_path,
+):
+    (tmp_path / "workflow").write_text("")
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nLOAD_WORKFLOW <CONFIG_PATH>/workflow\n"
+    )
+    assert [
+        x
+        for x in make_suggestion_list(str(tmp_path / "config.ert"))
+        if "DATA_KW" not in x and "DEFINE" not in x
+    ] == []
+
+
+def test_that_suggester_gives_schedule_prediciton_migration(tmp_path):
+    (tmp_path / "config.ert").write_text(
+        "NUM_REALIZATIONS 1\nSCHEDULE_PREDICTION_FILE no no no\n"
+    )
+    suggestions = make_suggestion_list(str(tmp_path / "config.ert"))
+
+    assert any(
+        "The 'SCHEDULE_PREDICTION_FILE' config keyword has been removed" in s
+        for s in suggestions
+    )
